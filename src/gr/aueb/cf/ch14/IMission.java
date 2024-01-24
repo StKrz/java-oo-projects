014130ecc7087d8268dcb2e8911aedeb79efe793
StKrz <stavroskyriazopoulos@gmail.com>
2024-01-24 04:52:41 +0200

    Singleton class

---
+++ src/gr/aueb/cf/ch14/IMission.java
@@ -0,0 +1,5 @@
+package gr.aueb.cf.ch14;
+@FunctionalInterface
+public interface IMission {
+    void embark();
+}
---
+++ src/gr/aueb/cf/ch14/SaintGeorgeKnight.java
@@ -0,0 +1,17 @@
+package gr.aueb.cf.ch14;
+
+/**
+ * Defines a Singleton SaintGeorgeKnight that
+ * embarks on mission.
+ */
+public class SaintGeorgeKnight {
+    private static final SaintGeorgeKnight INSTANCE = new SaintGeorgeKnight();
+
+    private SaintGeorgeKnight() {}
+
+    public static SaintGeorgeKnight getInstance() {
+        return INSTANCE;
+    }
+
+    public void embarkOnMission(IMission mission) {mission.embark();}
+}
