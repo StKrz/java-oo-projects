96f108ce5315d0dc91b1186f99a2e971e71be18d
Stavros Kyriazopoulos <148552307+StKrz@users.noreply.github.com>
2024-03-04 18:19:28 +0200

    Delete src/gr/aueb/cf/ch16/UML2/shapes directory

--- src/gr/aueb/cf/ch16/UML2/shapes/Line.java
+++
@@ -1,8 +0,0 @@
-package gr.aueb.cf.ch16.UML2.shapes;
-
-import gr.aueb.cf.ch16.UML2.AbstractShape;
-import gr.aueb.cf.ch16.UML2.ILine;
-
-public class Line extends AbstractShape implements ILine {
-    private double length;
-}
